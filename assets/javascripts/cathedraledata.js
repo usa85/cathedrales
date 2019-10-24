c718ebacae2a964706a20eabcdfc2c63ccda39e4
Pierre J. Callies <35848450+usa85@users.noreply.github.com>
2019-10-24 14:28:32 -0500

    with text loading

--- assets/javascripts/cathedraledata.js
+++ assets/javascripts/cathedraledata.js
@@ -7,7 +7,28 @@ let cathedraled=[
         "type": "Cathedrale",
         "epoque": "Romano-Gothique",
         "primaryID": 1,
-        "description": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis sollicitudin porttitor nibh. Sed viverra ex mi, sit amet dictum mi dictum in. "
+        "description": "<h3>Introduction</h3><p>The Basilique Ste-Madeleine (Basilica Church of St. Mary Magdalene) in Vézelay is the largest Romanesque church in France and only 10 yards shorter than the Notre-Dame Cathedral in Paris. As it claimed to guard the relics of St. Mary Magdalen, Vézelay was a major medieval pilgrimage destination. It also saw the launch of the Second and Third Crusades.</p><h3>History of Vézelay Abbey.</h3><p>Vezelay history began in 860 AD, when the hilltop site was donated for the purposes of a monastery by Gerard, Count of Roussillon and his wife, Bertha. King Nicholas I in 867 and King Charles the Bald in 868 confirmed the donation.</p><p>The new Vézelay abbey was overseen by the great Benedictine abbey at Cluny. Eudes, the monastery's first abbot, offered hospitality to King John VIII, who in 879 consecrated the first church. Norman invasions destroyed the original church, which was then restored under Abbot Geoffrey in the early 11th century.</p> <p>It was also under Abbot Geoffrey that the abbey at Vézelay was first associated with St. Mary Magdalene. A papal letter dated to 1050 AD shows that the name of the saint was part of the official title of the abbey by that time.</p><p>It was around this time that the monks of Vézelay recorded an account according to which the tombs of Sts. Maximinus and Magdalen, at St-Maximin in Provence, had been opened and their bodies removed to Vézelay. Shortly afterwards, a second account relates that only the body of St. Magdalen was taken.</p><p> For two centuries the account of the monks of Vézelay was accepted. Papal bulls of Lucius III, Urban III, and Clement III confirmed the statement that they possessed the body of St. Mary Magdalen. Accordingly, the tomb of the saint was visited in the 12th century by a host of illustrious pilgrims. <strong>All France</strong>, wrote Hugh of Poitiers, seems to go to the solemnities of the Magdalen.</p><p>The pilgrimage led to the town of Vezelay rising up around the abbey and an increase in the monastery's power and prestige. Contruction on the present basilica began in 1096 under Abbot Artaud to properly honor the sacred relics and welcome the many pilgrims. The Basilica of the Madeleine was dedicated in 1104 by Paschal II, Artaud's successor.</p> <p>Soon after the founding of the original basilica, major conflict erupted. Abbot Artaud demanded money from the townspeople for the reconstruction of the church and the monks refused to grant political independence to the citizens. This resulted in an insurrection in July 1120 in which the abbey was burnt and the abbot murdered.</p>"
+        
+        // Abbot Renaud de Semur, who later became Archbishop of Lyons, raised the basilica from ruins and added an abbot's palace. Work on the basilica's Romanesque nave was underway from 1120 to its dedication in 1132; the narthex was built around 1140-50. The original choir was destroyed by fire in 1165 and rebuilt in the Gothic style.
+        
+        // During the 12th century three more revolts occurred, fanned by the counts of Nevers, who wished to acquire the suzerainty over Vezelay for themselves. The monks were aided by the influence both of the Pope and of King Louis VII, however, and came out victorious in every instance.
+        
+        // Under Abbot Pontius of Montboisier (d. 1161), a former monk of Cluny, Vézelay emancipated itself from Cluniac rule, declared its autonomy as against the claims of the bishops of Autun, and victoriously resisted the encroachments of the counts of Nevers.
+        
+        // Despite these conflicts, Vézelay continued to receive thousands of pilgrims and it hosted a number of important historical events:
+        
+        // The prestige of the abbey began to diminish in 1280 when the Dominicans of St. Maximin in Provence claimed that the true body of St. Mary Magdalene had been discovered in their church. Consequently, the number of pilgrims to Vézelay declined sharply during the 14th and 15th centuries.
+        
+        // In 1538, a papal Bull of Secularization sought by King Francis I and the monks themselves transformed the abbey into a simple collegiate church. Odet de Chatillon, brother of Coligny and Abbot of Vézelay, subsequently became a Calvinist.
+        
+        // During the 16th century Wars of Religion, Vézelay was thoroughly sacked by Huguenots. The Huguenot masters of Vézelay converted the Madeleine into a storehouse and stable and burned the relics.
+        
+        // <p>During the French Revolution the ancient monastery buildings were destroyed and sold at auction. Only the basilica, cloister, and dormitory escaped demolition.</p>
+        
+        // <p>An attempt at restoration of the once-great pilgrimage site was made in 1876 by the future Cardinal Bernadou, Archbishop of Sens. The archbishop determined to restore the pilgrimage of St. Mary Magdalen at Vézelay and so brought a relic of the saint which Martin IV had given to the Chapter of Sens in 1281.</p>
+        
+        // <p>The basilica itself was restored by Viollet le Duc in 1840, the same restorer who fixed up the cathedrals of Laon, Amiens and Paris's Notre-Dame.</p>
+ 
     },
 
     {
@@ -15,10 +36,10 @@ let cathedraled=[
         "noms": "Saint Philibert",
         "latitude": 46.59167,
         "longitude": 4.9092,
-        "type": "Cathedrale",
+        "type": "Abbey",
         "epoque": "Romano",
         "primaryID": 2,
-        "description": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis sollicitudin porttitor nibh. Sed viverra ex mi, sit amet dictum mi dictum in. "
+        "description": "<p>The Abbey of <strong>Saint Philibert</strong> in the riverside town of Tournus, Burgundy, is a fortress-like Romanesque church with many interesting features. The church tends to be uncrowded and is well worth a visit.</p><p>Dating mainly from the 11th century with a 10th-century crypt, it boasts an impressively tall nave with an unusual vault, carved capitals, an important Romanesque statue of the Virgin and Child, and newly-discovered 12th-century floor mosaics depicting the zodiac.</p><h3>History of Tournus Abbey</h3><p>In Roman times, Tournus was a small fortified town built alongside the River Saône. The first documented presence of Christianity is in the 2nd century, with the arrival of St. Valerian from Lyon. Valerian preached in Tournus and converted some of its inhabitants before being executed by the Romans around 179 AD. His tomb became a secret place of pilgrimage for early Christians.</p><p>In the 4th century, after Christianity became legal under Emperor Constantine, a public oratory was built over the tomb. A small monastery dedicated to St. Valerian was founded on the site in the 6th century. This and other early buildings were badly damaged in Arab raids in 731 and partially rebuilt afterwards.</p><p>In 875, King Charles the Bald offered the Abbey of St. Valerian in Tournus to homeless monks from Noirmoutier, whose monastery had been captured by the Normans. That monastery had been founded by St. Philibert (616-85), whose relics the monks carried with them. This led to an unusual situation in which the abbey was shared by two monastic communities, each dedicated to their own saint.</p>"
     },
 
     {
@@ -29,7 +50,7 @@ let cathedraled=[
         "type": "Abbaye",
         "epoque": "Romano",
         "primaryID": 3,
-        "description": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis sollicitudin porttitor nibh. Sed viverra ex mi, sit amet dictum mi dictum in. "
+        "description": "<p>The Abbey of Clunyin Burgundy was the center of a major monastic movement in the Middle Ages. Its church was the largest Christian building in the world until St. Peter's Basilica was rebuilt in Rome in the 16th century.</p><p>Today, one transept of the 12th-century abbey church remains, along with 15th-century abbots' residences and 18th-century convent buildings. The site also includes pleasant gardens and a museum with Romanesque artifacts.</p> <h3>History of Cluny Abbey</h3><p>Founded in 910, the Abbey at Cluny was the center of a monastic reform movement that would spread throughout Europe. The abbey was built on a forested hunting preserve donated by William I the Pious, duke of Aquitaine and count of Auvergne.</p><p>Unlike most monastic patrons, William relieved the monks of Cluny of all obligations to him except for their prayers for his soul. It was much more common for patrons to retain some proprietary interest in the abbey and they usually expected to install their relatives as abbots. Thus Cluny was able to avoid the secular entanglements that plagued many other monasteries. Cluny answered to the Pope alone, and would come to develop very close ties with the papacy.</p>"
     },
 
     {
--- assets/javascripts/loadingcathedraledata.js
+++ assets/javascripts/loadingcathedraledata.js
@@ -1,11 +1,31 @@
+let basedescription;
+basedescription = document.querySelector("#content").innerHTML;
+
+function clickfunction(id){
+    console.log(id);
+    let content = document.querySelector("#content");
+    content.innerHTML= cathedraled[id].description; 
+    // let picture = document.createElement("img")
+    // picture.src=cathedraled[id].pictureURL;
+    // content.appendChild(picture);
+};
+
 document.addEventListener("DOMContentLoaded", () => {
+
+   let description=document.querySelector("#descriptionHeader");
+    description.addEventListener('click', () => {
+        let content = document.querySelector("#content");
+        content.innerHTML=basedescription;
+    });
 
     for (i = 0; i < cathedraled.length; i++) {
         let cathlist = document.querySelector("#cathlist");
         //console.log(list);
         //console.log(cenotaph2[i].name);
+
         let cathelement = document.createElement("li");
-        cathelement.innerText = cathedraled[i].noms + " - " + cathedraled[i].lieux;
+
+        cathelement.innerHTML = '<span onclick="clickfunction('+ (cathedraled[i].primaryID-1) + ')">' + cathedraled[i].noms + " <br><i>" + cathedraled[i].lieux +"</i>" + '</span>';
         console.log(cathelement);
 
 
