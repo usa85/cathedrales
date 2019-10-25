ce2db82366ec9fc2fe278820559f08944239f038
Pierre J. Callies <35848450+usa85@users.noreply.github.com>
2019-10-24 23:19:58 -0500

    with Rennes

--- assets/javascripts/cathedraledata.js
+++ assets/javascripts/cathedraledata.js
@@ -259,6 +259,7 @@ let cathedraled=[
         "type": "Cathedrale",
         "epoque": "Romano-Gothique",
         "primaryID": 22,
+        "sieclesdebut": 1210,
         "description": "description va ici "
     },
 
@@ -270,6 +271,7 @@ let cathedraled=[
         "type": "Chateau",
         "epoque": "Romano-Gothique",
         "primaryID": 23,
+        "sieclesdebut": 1210,
         "description": "description va ici "
     },
 
@@ -281,6 +283,7 @@ let cathedraled=[
         "type": "Sanctuaire",
         "epoque": "Romano-Gothique",
         "primaryID": 24,
+        "sieclesdebut": 1210,
         "description": "description va ici "
     },
 
@@ -292,6 +295,7 @@ let cathedraled=[
         "type": "Chateau",
         "epoque": "Romano-Gothique",
         "primaryID": 25,
+        "sieclesdebut": 1210,
         "description": "description va ici"
     },
 
@@ -303,6 +307,7 @@ let cathedraled=[
         "type": "Chateau",
         "epoque": "Romano-Gothique",
         "primaryID": 26,
+        "sieclesdebut": 1210,
         "description": "description va ici"
     },
 
@@ -314,6 +319,7 @@ let cathedraled=[
         "type": "Abbeye",
         "epoque": "Romano-Gothique",
         "primaryID": 27,
+        "sieclesdebut": 1210,
         "description": "description va ici "
     },
 
@@ -325,6 +331,7 @@ let cathedraled=[
         "type": "Cathedrale",
         "epoque": "Romano-Gothique",
         "primaryID": 28,
+        "sieclesdebut": 1210,
         "description": "description va ici "
     },
 
@@ -336,6 +343,7 @@ let cathedraled=[
         "type": "Cathedrale",
         "epoque": "Romano-Gothique",
         "primaryID": 29,
+        "sieclesdebut": 1210,
         "description": "description va ici "
     },
 
@@ -347,6 +355,7 @@ let cathedraled=[
         "type": "Eglise",
         "epoque": "Romano-Gothique",
         "primaryID": 30,
+        "sieclesdebut": 1210,
         "description": "description va ici "
     },
 
@@ -358,6 +367,7 @@ let cathedraled=[
         "type": "Eglise",
         "epoque": "Gothique",
         "primaryID": 31,
+        "sieclesdebut": 1210,
         "description": "description va ici  "
     },
 
@@ -369,6 +379,7 @@ let cathedraled=[
         "type": "Eglise",
         "epoque": "Romano",
         "primaryID": 32,
+        "sieclesdebut": 1210,
         "description": "description va ici "
     },
 
@@ -380,6 +391,7 @@ let cathedraled=[
         "type": "Basilique",
         "epoque": "Romano-Gothique",
         "primaryID": 33,
+        "sieclesdebut": 1210,
         "description": "description va ici  "
     },
 
@@ -391,6 +403,7 @@ let cathedraled=[
         "type": "Curiosite",
         "epoque": "Napoleoniene",
         "primaryID": 34,
+        "sieclesdebut": 1210,
         "description": "description va ici "
     },
 
@@ -402,6 +415,182 @@ let cathedraled=[
         "type": "Eglise",
         "epoque": "Napoleoniene",
         "primaryID": 35,
+        "sieclesdebut": 1210,
         "description": "description va ici"
-    }
+    },
+    {
+        "lieux": "Sees",
+        "noms": "Notre-Dame de Sees",
+        "latitude": 48.605278,
+        "longitude": 0.179056,
+        "type": "Cathedrale",
+        "epoque": "Gothique",
+        "primaryID": 36,
+        "sieclesdebut": 1210,
+        "description": "<p>Originellement consacrée à saint Gervais et saint Protais, est une cathédrale de style gothique, située à Sées, près d'Alençon dans l'Orne, qui est le siège du diocèse correspondant à ce département.</p>"
+    },
+    {
+        "lieux": "Verdun",
+        "noms": "Notre-Dame de Verdun",
+        "latitude": 49.159560,
+        "longitude": 5.382460,
+        "type": "Cathedrale-Basilique",
+        "epoque": "Roman-Gothique",
+        "primaryID": 37,
+        "sieclesdebut": 990,
+        "description": "<p>La cathédrale Notre-Dame de Verdun est une cathédrale catholique romaine située à Verdun, dans le département de la Meuse en région Grand Est. Siège épiscopal du diocèse de Verdun, elle est élevée à la dignité de basilique par le pape Pie XII en 1947.</p>"
+    },
+    {
+        "lieux": "Grenoble-Vienne",
+        "noms": "Saint-Maurice de Vienne",
+        "latitude": 45.524170,
+        "longitude": 4.873060,
+        "type": "Co-Cathedrale",
+        "epoque": "Roman-Gothique",
+        "primaryID": 38,
+        "sieclesdebut": 990,
+        "description": "<p>La cathédrale Saint-Maurice de Vienne dans le département français de l'Isère est l’ancienne cathédrale de l'archidiocèse de Vienne, mentionné dès 3141 et dont l'archevêché a été supprimé en 1790 lors de la Révolution française. Jusqu’à cette date, la cathédrale était également le siège de la Primatie des Sept Provinces. Elle est aujourd'hui co-cathédrale du Diocèse de Grenoble-Vienne.</p><p>La cathédrale Saint-Maurice fait l’objet d’un classement au titre des monuments historiques par la liste de 18402.</p>"
+    },
+
+    {
+        "lieux": "Le Mans",
+        "noms": "Saint-Julien du Mans",
+        "latitude": 48.009167,
+        "longitude": 0.198890,
+        "type": "Cathedrale",
+        "epoque": "Roman-Gothique Angevin",
+        "primaryID": 39,
+        "sieclesdebut": 1430,
+        "description": "<p>La cathédrale Saint-Julien est un édifice religieux situé dans la ville du Mans, dans la Sarthe. Elle est le symbole du diocèse et de l'évêché du Mans où siège l’évêque de la ville. Elle est l’un des plus grands édifices de l’époque gothique-romane de France et un cas unique dans l’Ouest. Elle est un témoignage médiéval du style architectural du gothique angevin.</p><P>La cathédrale du Mans a subi maintes constructions et reconstructions depuis sa fondation. Commencée vers 1060 par l’évêque Vulgrin, elle fut achevée sous sa forme actuelle vers 1430. Elle aurait dû être encore agrandie vers 1500, mais le manque de moyens fit se résigner les autorités religieuses de l’époque. Attaquée par la pollution de l'air au fil des ans, sujette au vieillissement naturel, la cathédrale Saint-Julien est un chantier quasi permanent de rénovation. Elle abrite les tombes de saint Julien et de Charles d’Anjou. Située sur la butte du Vieux-Mans, l'édifice possède une tour culminant à 64 mètres qui en fait l'édifice le plus haut de l'agglomération mancelle1 et dominant ainsi le Pays du Mans2.</p><p>Avec 283 613 visiteurs, la cathédrale Saint-Julien a été le monument le plus visité des Pays de la Loire en 20093.</p>"
+    },
+
+    {
+        "lieux": "Toulouse",
+        "noms": "Saint-Etienne de Toulouse",
+        "latitude": 43.599900,
+        "longitude": 1.450400,
+        "type": "Cathedrale",
+        "epoque": "Gothique Meridional",
+        "primaryID": 40,
+        "sieclesdebut": 1071,
+        "description": "<p>La cathédrale Saint-Étienne de Toulouse est une cathédrale catholique romaine du centre historique de Toulouse, dans le département de la Haute-Garonne. Elle a donné son nom au quartier qui l'entoure. On ne connaît pas les origines de la cathédrale. Ses premières traces datent de 1071, quand l'évêque Isarn décida de reconstruire l'édifice, alors en ruine. Son architecture est particulière car elle est composée d'éléments architecturaux de diverses époques. La cathédrale jouxte l'ancien palais épiscopal, aujourd'hui occupé par la préfecture.</p>"
+    },
+
+    {
+        "lieux": "Viviers",
+        "noms": "Saint-Vincent de Viviers",
+        "latitude": 44.481964,
+        "longitude": 4.6990406,
+        "type": "Cathedrale",
+        "epoque": "Gothique Meridional",
+        "primaryID": 41,
+        "sieclesdebut": 1119,
+        "description": "<p>La cathédrale Saint-Vincent de Viviers est la cathédrale du diocèse catholique de Viviers depuis le ive siècle. Elle est située à Viviers, dans le département de l'Ardèche, en France.</p><p>La date du transfert du siège d'épiscopal d'Alba, capitale des Helviens, et le nom de l'évêque ayant fait ce transfert, a fait l'objet d'un débat au xixe siècle. R. Lauxerois a repris l'avis de Louis Duchesne affirmant que ce transfert a été fait par un certain Promotus, et non Auxonius, 5e évêque d'Alba. L'histoire racontée au xixe siècle d'un évêque assassiné, d'une population en fuite, et d'un évêque s'installant à Mélas puis à Viviers, est fantaisiste. La Vetus Carta, premier cartulaire de la cathédrale de Viviers datant du xe siècle, donne dans la liste des évêques comme successeur de Promotus, Lucianus siégeant regnate Alarico, sous le règne d'Alaric II, mort en 507. On a trouvé un épitaphe d'un évêque mort en 487 à Saint-Thomé. Yves Esquieu en déduit que le transfert du siège épiscopal a bien été fait par Promotus, premier évêque installé à Viviers entre 474-475 et 487. Rien dans l'épitaphe et dans la Vetus Carta ne permet de savoir pourquoi le siège a été transféré. On ne sait pas à quelle date Alba a été abandonnée. Le transfert est probablement dû aux nouvelles conditions politiques et économiques. Après Promotus, il y a l'évêque Lucianus, puis vient Venance qui appartient à la famille des rois burgondes et qui a participé au concile d'Épaone en 517, et au concile de Clermont en 535. Il serait mort en 544. D'après les Acta sanctii Venantii, c'est l'évêque Venance qui aurait relevé les murailles de la cité, orné les églises, et restauré la cathédrale.</p><p>Il est possible que la cathédrale de Viviers ait été ruinée en 736 pendant une incursion de Sarrasins, puis reconstruite. C'est peut-être à la suite de cet évènement que l'implantation de la cathédrale a été déplacée vers le sud.</p><p>Quand l'évêque Léodégaire est élu en 1096, il trouve une cathédrale en ruine. Il entreprend sa reconstruction. La nouvelle cathédrale est consacrée le 27 février 1119 par le pape Calixte II.</p><p>Des fouilles ont été entreprises en 1978 et 1983, place de la Plaine, au nord de la cathédrale. Elles ont mis au jour des structures paléochrétiennes et les vestiges d'un groupe de trois édifices qui devaient composer le groupe cathédral du haut Moyen Âge1. Ces fouilles ont aussi permis de trouver les fondations du cloître des chanoines du xiiie siècle.</p>"
+    },
+
+    {
+        "lieux": "Saint - Malo",
+        "noms": "Saint-Vincent-de-Saragosse de Saint-Malo",
+        "latitude": 48.649490,
+        "longitude": -2.025648,
+        "type": "Cathedrale",
+        "epoque": "Roman, Gothique",
+        "primaryID": 42,
+        "sieclesdebut": 1146,
+        "description": "<p>La cathédrale Saint-Vincent-de-Saragosse de Saint-Malo est une ancienne cathédrale catholique romaine dédiée à saint Vincent de Saragosse, situé à Saint-Malo en Ille-et-Vilaine. Son architecture mélange les styles roman et gothique, et elle est classée monument historique de France1.</P><p> Elle a été le siège de l'ancien évêché de Saint-Malo depuis 1146. Ce dernier fut supprimé par le concordat de 1801, et son territoire réparti entre les diocèses de Rennes, de Saint-Brieuc et de Vannes.</p>"
+    },
+
+    {
+        "lieux": "Angoulême",
+        "noms": "Saint-Pierre d'Angoulême",
+        "latitude": 45.648889,
+        "longitude": 0.151667,
+        "type": "Cathedrale",
+        "epoque": "Roman, Gothique",
+        "primaryID": 43,
+        "sieclesdebut": 991,
+        "description": "<p>La cathédrale Saint-Pierre est une cathédrale de style roman située à Angoulême, dans le département de la Charente.</p>"
+    },
+
+    {
+        "lieux": "Angers",
+        "noms": "Saint-Maurice d'Angers",
+        "latitude": 47.470556,
+        "longitude": -0.555000,
+        "type": "Cathedrale",
+        "epoque": "Roman, Gothique Angevin",
+        "primaryID": 44,
+        "sieclesdebut": 1025,
+        "description": "<p>La cathédrale Saint-Maurice d'Angers, en France, est l'église cathédrale du diocèse d'Angers.</p><p> L'œuvre est intermédiaire entre les styles roman et gothique. La cathédrale est un témoignage de l'art gothique angevin.</p>"
+    },
+
+    {
+        "lieux": "Poitiers",
+        "noms": "Saint-Pierre de Poitiers",
+        "latitude": 46.580300,
+        "longitude": 0.349300,
+        "type": "Cathedrale",
+        "epoque": "Roman, Gothique Angevin",
+        "primaryID": 45,
+        "sieclesdebut": 1160,
+        "description": "<p>a cathédrale Saint-Pierre est une cathédrale catholique romaine située à Poitiers, dans le département de la Vienne et la région Nouvelle-Aquitaine. Elle est le siège de l'archidiocèse de Poitiers. En plus de son titre d'église archiépiscopale, elle a rang de basilique mineure depuis le 1er mars 1912. Moins connue que l'église Notre-Dame-la-Grande, cet immense vaisseau de pierre est pourtant le plus vaste édifice religieux de la ville et un repère visible de loin dans le paysage urbain.</p><p>Construite à l'initiative d'Aliénor d'Aquitaine et d'Henri II Plantagenêt à partir de 1160, consacrée en 1379, elle est de style gothique angevin (emploi de voûtes bombées sur plan carré) et s'apparente aux églises-halles par sa division en trois vaisseaux d'égale hauteur. La façade, cantonnée de deux tours inachevées, emprunte des éléments à la grammaire stylistique du nord de la France.</p><p>L'intérieur conserve des stalles du xiiie siècle et une collection de vitraux historiés datant des xiie et xiiie siècles, parmi lesquels une Crucifixion, comptant parmi les sommets de l'art du vitrail médiéval français.</p>"
+    },
+
+    {
+        "lieux": "Coutances",
+        "noms": "Notre-Dame de Coutances",
+        "latitude": 49.047500,
+        "longitude": -1.444200,
+        "type": "Cathedrale",
+        "epoque": "Gothique",
+        "primaryID": 46,
+        "sieclesdebut": 1200,
+        "description": "<p>La cathédrale Notre-Dame de Coutances est un édifice catholique romain du xiiie siècle de style gothique, siège du diocèse de Coutances et Avranches, situé dans le département français de la Manche, en région Normandie. Elle est affectueusement surnommée par les Coutançais: La cathédrale de fierté, comme le disait le poète local Louis Beuve dans Ma cathédrale de fierté ou La lettre à la morte.</p><p>Construite à une altitude de 90 mètres, elle domine la ville de Coutances. Elle est visible depuis la mer et même, selon la légende, de La Hougue Bie à Jersey, située pourtant à plus de 40 kilomètres[réf. nécessaire].</p><p>La cathédrale a pour spécificité le rhabillage de certaines parties de l'édifice roman qui l'a précédée par des éléments gothiques : sous le parement gothique de la nef et des tours de façade se dresse encore la structure romane de la cathédrale du xie siècle, qui est visible lorsque l'on parcourt les parties hautes, par exemple lors des visites-conférences.</p><p>Elle possède deux flèches en façade, et une tour-lanterne. Exemple typique du gothique normand, elle se caractérise par des lignes très pures.</p>"
+    },
+    {
+        "lieux": "Coutances",
+        "noms": "Notre-Dame de Coutances",
+        "latitude": 49.047500,
+        "longitude": -1.444200,
+        "type": "Cathedrale",
+        "epoque": "Gothique",
+        "primaryID": 47,
+        "sieclesdebut": 1200,
+        "description": "<p>La cathédrale Notre-Dame de Coutances est un édifice catholique romain du xiiie siècle de style gothique, siège du diocèse de Coutances et Avranches, situé dans le département français de la Manche, en région Normandie. Elle est affectueusement surnommée par les Coutançais: La cathédrale de fierté, comme le disait le poète local Louis Beuve dans Ma cathédrale de fierté ou La lettre à la morte.</p><p>Construite à une altitude de 90 mètres, elle domine la ville de Coutances. Elle est visible depuis la mer et même, selon la légende, de La Hougue Bie à Jersey, située pourtant à plus de 40 kilomètres[réf. nécessaire].</p><p>La cathédrale a pour spécificité le rhabillage de certaines parties de l'édifice roman qui l'a précédée par des éléments gothiques : sous le parement gothique de la nef et des tours de façade se dresse encore la structure romane de la cathédrale du xie siècle, qui est visible lorsque l'on parcourt les parties hautes, par exemple lors des visites-conférences.</p><p>Elle possède deux flèches en façade, et une tour-lanterne. Exemple typique du gothique normand, elle se caractérise par des lignes très pures.</p>"
+    },
+    {
+        "lieux": "Albi",
+        "noms": "Sainte-Cecile d'Albi",
+        "latitude":43.928490,
+        "longitude": 2.142945,
+        "type": "Cathedrale",
+        "epoque": "Gothique",
+        "primaryID": 48,
+        "sieclesdebut": 1480,
+        "description": "<p>La cathédrale Sainte-Cécile d'Albi est le siège de l'archidiocèse d'Albi, dans le département du Tarn en France. Elle est construite sur un piton rocheux qui domine le Tarn. Deux siècles auront été nécessaires pour son édification, de 1282 à 1480.</p>"
+    },
+
+    {
+        "lieux": "Quimper",
+        "noms": "Saint-Corentin de Quimper",
+        "latitude":47.995556,
+        "longitude": -4.102222,
+        "type": "Cathedrale",
+        "epoque": "Gothique",
+        "primaryID": 49,
+        "sieclesdebut": 1239,
+        "description": "<p>La cathédrale Saint‑Corentin, dont le nom complet est basilique cathédrale Saint-Corentin1, est un lieu de culte catholique sous le patronage de Notre-Dame et du premier évêque légendaire dont elle porte le nom, saint Corentin. Elle est située dans le centre-ville de Quimper, chef-lieu du département du Finistère, en Bretagne. Siège du diocèse de Quimper avant la Révolution française, le bâtiment est depuis 1801 le siège du diocèse de Quimper et Léon.</p>"
+    },
+
+    {
+        "lieux": "Rennes",
+        "noms": "Saint Pierre de Rennes",
+        "latitude":48.111389,
+        "longitude": -1.683333,
+        "type": "Cathedrale",
+        "epoque": "Classique",
+        "primaryID": 50,
+        "sieclesdebut": 1443,
+        "description": "<p>La cathédrale Saint-Pierre de Rennes est la cathédrale catholique romaine, située au cœur de la ville de Rennes, en Bretagne. Elle est le siège de l’archidiocèse de Rennes, Dol et Saint-Malo. De style classique, elle est l'une des neuf cathédrales historiques de Bretagne.</p>"
+    },
 ];
